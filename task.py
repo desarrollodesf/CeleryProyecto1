35f9ea314901a9203a0c76ae27f35e51adb3545b
nirobe <n.rozo10@uniandes.edu.co>
2022-02-14 20:19:00 -0500

    se agrega actualziacion de datos y transformacion de archivo

--- task.py
+++ task.py
@@ -1,5 +1,9 @@
 from celery import Celery
+from pathlib import Path
+from pydub import AudioSegment
 import requests
+import os
+import ffmpeg
 
 app = Celery( 'tasks' , broker = 'redis://localhost:6379/0' )
 
@@ -14,15 +18,24 @@ def check():
         email = form['email']
         name = form['name']
         lastname = form['lastname']
-        pathOriginal = form['original']
+        pathOriginal = form['original'].replace("/", "\\")   
+    
+        dir_name = os.path.dirname(pathOriginal)
+        input_file_name = os.path.basename(pathOriginal).split('.')[0] 
+        mp3_file = dir_name + "\\" + input_file_name + ".mp3"
+        cmd = "ffmpeg -y -i {} {}".format(pathOriginal, mp3_file)
+        os.system(cmd)
 
-        
-
-        print (id_form)
+        print(mp3_file)
+        print(mp3_file.replace("\\", "/"))
+        print('http://127.0.0.1:5000/api/form/' + str(id_form))
+        response = requests.put('http://127.0.0.1:5000/api/form/' + str(id_form),
+        json={"state": "Convertida", "formatted": mp3_file.replace("\\", "/") })
+        print(response)
 
 app.conf.beat_schedule ={
     "run-me-every-te-seconds": {
         "task" : "tasks.check",
-        "schedule" : 10.0
+        "schedule" : 30.0
     }
 }
