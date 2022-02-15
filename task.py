2d66da8380d4b5df12387e7e2c46aabecef77df6
nirobe <n.rozo10@uniandes.edu.co>
2022-02-15 00:02:55 -0500

    Update task.py

--- task.py
+++ task.py
@@ -4,14 +4,17 @@ from pydub import AudioSegment
 import requests
 import smtplib
 import os
+from urllib3.exceptions import InsecureRequestWarning
 
 app = Celery( 'tasks' , broker = 'redis://localhost:6379/0' )
 
 
 @app.task(name='tasks.check')
 def check():
-    URL = "http://127.0.0.1:5000/api/forms/pendingToConvert"
-    r = requests.get(url = URL)
+    URL = "https://172.24.41.204/api/forms/pendingToConvert"
+    requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+    r = requests.get(url = URL, verify=False)
     data = r.json()
 
     for form in data:
@@ -27,8 +30,8 @@ def check():
         cmd = "ffmpeg -y -i {} {}".format(pathOriginal, mp3_file)
         os.system(cmd)
 
-        response = requests.put('http://127.0.0.1:5000/api/form/' + str(id_form),
-        json={"state": "Convertida", "formatted": mp3_file})
+        response = requests.put('https://172.24.41.204/api/form/' + str(id_form),
+        json={"state": "Convertida", "formatted": mp3_file}, verify=False)
         print(response)
     
     
@@ -37,7 +40,7 @@ def check():
             smtp.starttls()
             smtp.ehlo()
 
-            smtp.login('rafaelroperolayton@gmail.com', 'PONERCONTRASEÑA')
+            smtp.login('nicofrost87@gmail.com', 'PONERCONTRASEÑA')
 
             subject = "Confirmacion de publicacion de audio"
             body = "Saludos, le informamos que el audio con el que esta participando en el concurso fue publicado satisfactoriamente"
