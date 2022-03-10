e16fcc7e96105305cef61f4080faaf4b0a03eda2
nirobe <n.rozo10@uniandes.edu.co>
2022-03-09 19:56:45 -0500

    actualizacion con nuevo servicio de correo

--- task.py
+++ task.py
@@ -7,13 +7,15 @@ import os
 from datetime import datetime 
 import pytz
 from urllib3.exceptions import InsecureRequestWarning
+import sendgrid
+from sendgrid.helpers.mail import Mail, Email, To, Content
 
 app = Celery( 'tasks' , broker = 'redis://localhost:6379/0' )
 
 
 @app.task(name='tasks.check')
 def check():
-    URL = "https://172.24.41.204/api/forms/pendingToConvert"
+    URL = "http://44.202.144.123/api/forms/pendingToConvert"
     #URL = "http://127.0.0.1:5000/api/forms/pendingToConvert"
     
     requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
@@ -34,28 +36,28 @@ def check():
         startConv = str(datetime.strptime(datetime.utcnow().replace(tzinfo=pytz.utc).astimezone(pytz.timezone("America/New_York")).strftime('%Y-%m-%dT%H:%M:%S'),'%Y-%m-%dT%H:%M:%S'))
         cmd = "ffmpeg -y -i {} {}".format(pathOriginal, mp3_file)
         os.system(cmd)
+
+        subject = "Confirmacion de publicacion de audio"
+        body = "Saludos, le informamos que el audio con el que esta participando en el concurso fue publicado satisfactoriamente"
+        msg = f'Subject: {subject}\n\n{body}'
+        sg = sendgrid.SendGridAPIClient(api_key='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')
+        from_email = Email('n.rozo10@uniandes.edu.co')  # Change to your verified sender
+        to_email = To(email)  # Change to your recipient
+        mail = Mail(from_email, to_email, subject, msg)
+
+        # Get a JSON-ready representation of the Mail object
+        mail_json = mail.get()
+
+        # Send an HTTP POST request to /mail/send
+        mailresponse = sg.client.mail.send.post(request_body=mail_json)
+        print(mailresponse.status_code)
+        print(mailresponse.headers)
         endConv = str(datetime.strptime(datetime.utcnow().replace(tzinfo=pytz.utc).astimezone(pytz.timezone("America/New_York")).strftime('%Y-%m-%dT%H:%M:%S'),'%Y-%m-%dT%H:%M:%S'))
 
-        response = requests.put('https://172.24.41.204/api/form/' + str(id_form),
+        response = requests.put('http://144.202.144.123/api/form/' + str(id_form),
         #response = requests.put('http://127.0.0.1:5000/api/form/' + str(id_form),
         json={"state": "Convertida", "formatted": mp3_file, "startConversion": startConv, "finishConversion": endConv}, verify=False)
         print(response)
-    
-    
-        with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-            smtp.ehlo()
-            smtp.starttls()
-            smtp.ehlo()
-
-            smtp.login('cloudgrupo13@gmail.com', 'ormeefmgvsiwifzp')
-
-            subject = "Confirmacion de publicacion de audio"
-            body = "Saludos, le informamos que el audio con el que esta participando en el concurso fue publicado satisfactoriamente"
-
-            msg = f'Subject: {subject}\n\n{body}'
-
-            smtp.sendmail('cloudgrupo13@gmail.com', email, msg)
-
 
 app.conf.beat_schedule ={
     "run-me-every-te-seconds": {
